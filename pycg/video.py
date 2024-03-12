7d9a28986ee26423a3b988c4c3dab93e73eda559
heiwang1997 <462149548@qq.com>
2024-03-12 16:45:15 -0700

    update

--- pycg/video.py
+++ pycg/video.py
@@ -19,7 +19,7 @@ def make_video(input_format: str, output_path: str, fps: int):
 
 
 def make_video_xw264(input_format: str, output_path: str, fps: int = None, crf: float = 23.5,
-                     start_idx: Union[int] = None, end_idx: Union[int] = None):
+                     start_idx: Union[int] = None, end_idx: Union[int] = None, ffmpeg: bool = True):
     """
     Call the x264 program with a best set of parameters found by Maruto-toolbox.
         Note that this does not support audio yet!
@@ -27,6 +27,7 @@ def make_video_xw264(input_format: str, output_path: str, fps: int = None, crf: 
     :param output_path: output mp4 path
     :param crf: (float) the lower, the larger the file size and the better the video quality.
     :param fps: (int) frame-per-second
+    :param ffmpeg: (bool) use ffmpeg instead of x264
     """
     if isinstance(input_format, str):
         input_format = Path(input_format)
@@ -61,20 +62,29 @@ def make_video_xw264(input_format: str, output_path: str, fps: int = None, crf: 
     if len(missing_indices) > 0:
         print(f"Missing {len(missing_indices)} of {len(full_indices)} files.")
 
-    # --vf crop:0,0,1,1 
-    XW_ARGS = "--preset 8 -I 250 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 " + \
-              "--psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8"
+    if not ffmpeg:
+        cmdlines_args = f"x264 --crf {crf:.1f} " + \
+                        (f" --fps {fps} " if fps is not None else "") + \
+                        "--preset 8 -I 250 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 " + \
+                        "--psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 " + \
+                        "-o {output_path} {input_format}"
+        
+    else:
+        cmdlines_args = "ffmpeg " + \
+                        (f"-framerate {fps} " if fps is not None else "") + \
+                        "-i {input_format} " + \
+                        "-c:v libx264 -preset veryslow -crf 23.5 -g 250 -bf 3 -sc_threshold 60 " + \
+                        "-qcomp 0.5 -psy-rd 0.3:0 -aq-mode 2 -aq-strength 0.8 -me_method umh -flags +cgop " + \
+                        "-pix_fmt yuv420p -movflags +faststart " + \
+                        '-vf "crop=floor(iw/2)*2:floor(ih/2)*2" ' + \
+                        "{output_path}"
 
     if len(input_named_files) == 1:
-        os.system(f"x264 --crf {crf:.1f} " + XW_ARGS + \
-                  (f" --fps {fps}" if fps is not None else "") + \
-                  f" -o {output_path} {input_named_files[0][0]}")
+        os.system(cmdlines_args.format(input_format=input_named_files[0][0], output_path=output_path))
         return
 
     # Copy to temp location.
     with tempfile.TemporaryDirectory() as video_tmp_dir:
         for idx, (fname, _) in enumerate(tqdm.tqdm(input_named_files)):
             shutil.copy(fname, Path(video_tmp_dir) / f"{idx:06d}.png")
-        assert fps is not None
-        os.system(f"x264 --crf {crf:.1f} --fps {fps} " + XW_ARGS +
-                  f" -o {output_path} {video_tmp_dir}/%06d.png")
+        os.system(cmdlines_args.format(input_format=f"{video_tmp_dir}/%06d.png", output_path=output_path))
