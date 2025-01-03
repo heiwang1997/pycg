97a832c16ce8c3a6ac70040499de86a03ec5e139
Jiahui Huang <jiahuih@nvidia.com>
2025-01-03 10:43:52 -0800

    bump version

--- pycg/__init__.py
+++ pycg/__init__.py
@@ -9,7 +9,7 @@ from .isometry import Isometry, ScaledIsometry
 
 __author__ = "Jiahui Huang"
 __license__ = "MIT"
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 
 __all__ = ["Isometry", "ScaledIsometry"]
 
